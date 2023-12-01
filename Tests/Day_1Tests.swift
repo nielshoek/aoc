c16a0b9cc1356f7d13eb5ea251a783c7405c5b51
MadNelis <njhoek@avans.nl>
2023-12-01 12:52:50 +0100

    Commented out tests
    They are invalid

--- Tests/Day_1Tests.swift
+++ Tests/Day_1Tests.swift
@@ -1,66 +1,66 @@
-import sandbox
-import XCTest
+// import sandbox
+// import XCTest
 
-class Day_1ATests: XCTestCase {
-    func test1() {
-        // Arrange
-        let input = [1, 2, 3, 4, 5]
-        let expectedResult = 4
+// class Day_1ATests: XCTestCase {
+//     func test1() {
+//         // Arrange
+//         let input = [1, 2, 3, 4, 5]
+//         let expectedResult = 4
 
-        // Act
-        let result = Day_1.LogicA(input: input)
+//         // Act
+//         let result = Day_1.LogicA(input: input)
 
-        // Assert
-        XCTAssertEqual(expectedResult, result)
-    }
+//         // Assert
+//         XCTAssertEqual(expectedResult, result)
+//     }
 
-    func test2() {
-        // Arrange
-        let input = [1, 2, 2, 2, 2]
-        let expectedResult = 1
+//     func test2() {
+//         // Arrange
+//         let input = [1, 2, 2, 2, 2]
+//         let expectedResult = 1
 
-        // Act
-        let result = Day_1.LogicA(input: input)
+//         // Act
+//         let result = Day_1.LogicA(input: input)
 
-        // Assert
-        XCTAssertEqual(expectedResult, result)
-    }
+//         // Assert
+//         XCTAssertEqual(expectedResult, result)
+//     }
 
-    func test3() {
-        // Arrange
-        let input = [1, 1]
-        let expectedResult = 0
+//     func test3() {
+//         // Arrange
+//         let input = [1, 1]
+//         let expectedResult = 0
 
-        // Act
-        let result = Day_1.LogicA(input: input)
+//         // Act
+//         let result = Day_1.LogicA(input: input)
 
-        // Assert
-        XCTAssertEqual(expectedResult, result)
-    }
-}
+//         // Assert
+//         XCTAssertEqual(expectedResult, result)
+//     }
+// }
 
-class Day_1BTests: XCTestCase {
-    func test1() {
-        // Arrange
-        let input = [1, 1, 1, 2, 3, 4]
-        let expectedResult = 3
+// class Day_1BTests: XCTestCase {
+//     func test1() {
+//         // Arrange
+//         let input = [1, 1, 1, 2, 3, 4]
+//         let expectedResult = 3
 
-        // Act
-        let result = Day_1.LogicB(input: input)
+//         // Act
+//         let result = Day_1.LogicB(input: input)
 
-        // Assert
-        XCTAssertEqual(expectedResult, result)
-    }
+//         // Assert
+//         XCTAssertEqual(expectedResult, result)
+//     }
 
-    func test2() {
-        // Arrange
-        let input = [1, 1, 2, 1, 3, 4]
-        let expectedResult = 2
+//     func test2() {
+//         // Arrange
+//         let input = [1, 1, 2, 1, 3, 4]
+//         let expectedResult = 2
 
-        // Act
-        let result = Day_1.LogicB(input: input)
+//         // Act
+//         let result = Day_1.LogicB(input: input)
 
-        // Assert
-        XCTAssertEqual(expectedResult, result)
-    }
-}
+//         // Assert
+//         XCTAssertEqual(expectedResult, result)
+//     }
+// }
