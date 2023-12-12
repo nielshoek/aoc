46e8de8ba3ad7c3a0341bc04769bb35d0822c6d4
Niels Hoek <njhoek@gmail.com>
2023-12-12 23:40:39 +0100

    Day 11 part 2 (WIP)

--- Sources/Day11.swift
+++ Sources/Day11.swift
@@ -11,8 +11,8 @@ struct GalaxyPoint {
     let col: Int
 
     func distance(from other: GalaxyPoint) -> Int {
-        let xDiff = abs(self.row - other.row)
-        let yDiff = abs(self.col - other.col)
+        let xDiff = abs(row - other.row)
+        let yDiff = abs(col - other.col)
         return xDiff + yDiff
     }
 }
@@ -22,8 +22,8 @@ public struct Day11 {
         print()
         let data = "Inputs/day11.txt".ToStringArray()
         print("Part test: \(LogicA(input: testMap))")
-        print("Part 1: \(LogicA(input: data))")
-        // print("Part 2: \(LogicB(input: data))")
+        // print("Part 1: \(LogicA(input: data))")
+        print("Part 2: \(LogicB(input: testMap))")
     }
 
     public func LogicA(input: [String]) -> Int {
@@ -32,11 +32,12 @@ public struct Day11 {
         }
         grid = expandGrid(grid)
         let galaxies = extractGalaxyPoints(grid)
-
+// grid.print()
         var distanceAccumulator = 0
         for (i, currentGalaxy) in galaxies.enumerated() {
             for otherGalaxy in galaxies.dropFirst(i) {
                 let distance = currentGalaxy.distance(from: otherGalaxy)
+                print("A: \(distance)")
                 distanceAccumulator += distance
             }
         }
@@ -44,8 +45,23 @@ public struct Day11 {
         return distanceAccumulator
     }
 
-    public func LogicB(input _: [String]) -> Int {
-        0
+    public func LogicB(input: [String]) -> Int {
+        var grid = input.reduce(into: [[Character]]()) { acc, cur in
+            acc.append(cur.map(Character.init))
+        }
+        grid = expandGrid2(grid)
+        let galaxies = extractGalaxyPoints(grid)
+// grid.print()
+        var distanceAccumulator = 0
+        for (i, currentGalaxy) in galaxies.enumerated() {
+            for otherGalaxy in galaxies.dropFirst(i) {
+                let distance = currentGalaxy.distance(from: otherGalaxy)
+                print("B: \(distance)")
+                distanceAccumulator += distance
+            }
+        }
+
+        return distanceAccumulator
     }
 
     private func extractGalaxyPoints(_ grid: [[Character]]) -> [GalaxyPoint] {
@@ -92,6 +108,51 @@ public struct Day11 {
                 let part = gridWithDoubledRows[rowIndex][lastCol ..< i]
                 result[rowIndex].append(contentsOf: part)
                 result[rowIndex].append(".")
+            }
+            lastCol = i
+        }
+        for rowIndex in 0 ..< gridWithDoubledRows.count {
+            let part = gridWithDoubledRows[rowIndex][lastCol ..< gridWithDoubledRows[rowIndex].count]
+            result[rowIndex].append(contentsOf: part)
+        }
+
+        return result
+    }
+
+    private func expandGrid2(_ originalGrid: [[Character]]) -> [[Character]] {
+        let EXPAND_FACTOR = 2
+        var result = [[Character]]()
+        var colsToExpand = [Int]()
+        outer: for i in 0 ..< originalGrid[0].count {
+            for row in originalGrid {
+                if row[i] == "#" {
+                    continue outer
+                }
+            }
+            colsToExpand.append(i)
+        }
+
+        var gridWithDoubledRows = [[Character]]()
+        for (i, row) in originalGrid.enumerated() {
+            if row.contains("#") {
+                gridWithDoubledRows.append(row)
+            } else {
+                for _ in 0 ..< EXPAND_FACTOR {
+                    gridWithDoubledRows.append(row)
+                }
+            }
+        }
+
+        for _ in 0 ..< gridWithDoubledRows.count {
+            result.append([Character]())
+        }
+
+        var lastCol = 0
+        for i in colsToExpand {
+            for rowIndex in 0 ..< gridWithDoubledRows.count {
+                let part = gridWithDoubledRows[rowIndex][lastCol ..< i]
+                result[rowIndex].append(contentsOf: part)
+                result[rowIndex].append(contentsOf: [Character](repeating: ".", count: EXPAND_FACTOR - 1))
             }
             lastCol = i
         }
