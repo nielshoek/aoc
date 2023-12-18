4143b4c1a8edb154c391544a34c564b3e43c8170
Niels Hoek <njhoek@gmail.com>
2023-12-18 18:48:59 +0100

    Day 13 part 2

--- Sources/Day13.swift
+++ Sources/Day13.swift
@@ -1,25 +1,8 @@
-let test13 = [
-    "#.######.#.", // 1
-    "....##.#...", // 2
-    "..######...", // 3
-    "###....###.", // 4
-    "#..####..##", // 5
-    ".#.#..#.#.#", // 6
-    ".#.#..#.#.#", // 7
-    "##......###", // 8
-    "..........#", // 9
-    ".#.####.#.#", // 10
-    ".#......#.#", // 11
-    ".#......#.#", // 12
-    ".#.####.#.#", // 13
-]
-
 public struct Day13 {
     public func Run() {
         let data = "Inputs/day13.txt".ToStringArray()
-        print("Part Test: \(LogicA(input: test13))")
         print("Part 1: \(LogicA(input: data))")
-        // print("Part 2: \(LogicB(input: data))")
+        print("Part 2: \(LogicB(input: data))")
     }
 
     public func LogicA(input: [String]) -> Int {
@@ -35,61 +18,95 @@ public struct Day13 {
         return totalScore
     }
 
+    public func LogicB(input: [String]) -> Int {
+        let blocks = input.split(separator: "")
+
+        var totalScore = 0
+        for block in blocks {
+            let rowScore = getRowScore2(block: Array(block))
+            let colScore = getColScore2(block: Array(block))
+            totalScore += rowScore + colScore
+        }
+
+        return totalScore
+    }
+
     private func getRowScore(block: [String]) -> Int {
-        var result = 0
         for i in 0 ..< block.count - 1 {
-            let row1 = block[i]
-            let row2 = block[i + 1]
-            if row1 == row2 {
-                var i1 = i - 1
-                var i2 = i + 2
-                var isMirror = true
-                while i1 >= 0 && i2 < block.count {
-                    if block[i1] == block[i2] {
-                        i1 -= 1
-                        i2 += 1
-                    } else {
-                        isMirror = false
-                        break
-                    }
-                }
-                if isMirror {
-                    return (i + 1) * 100
-                }
+            let rowDiff = getRowDiff(block: block, i1: i, i2: i + 1)
+            if rowDiff == 0 {
+                return (i + 1) * 100
             }
         }
 
-        return result
+        return 0
     }
 
     private func getColScore(block: [String]) -> Int {
-        var result = 0
         for i in 0 ..< block[0].count - 1 {
-            var col1 = i
-            var col2 = i + 1
-            var isMirror = true
-            outer: while col1 >= 0 && col2 < block[0].count {
-                for j in 0 ..< block.count {
-                    let row = block[j]
-                    if row[col1] != row[col2] {
-                        isMirror = false
-                        break outer
-                    }
-                }
-                col1 -= 1
-                col2 += 1
-            }
-
-            if isMirror {
+            let colDiff = getColDiff(block: block, i1: i, i2: i + 1)
+            if colDiff == 0 {
                 return i + 1
             }
         }
 
-        return result
+        return 0
     }
 
-    public func LogicB(input _: [String]) -> Int {
-        0
+    private func getRowScore2(block: [String]) -> Int {
+        for i in 0 ..< block.count - 1 {
+            let rowDiff = getRowDiff(block: block, i1: i, i2: i + 1)
+            if rowDiff == 1 {
+                return (i + 1) * 100
+            }
+        }
+
+        return 0
+    }
+
+    private func getColScore2(block: [String]) -> Int {
+        for i in 0 ..< block[0].count - 1 {
+            let colDiff = getColDiff(block: block, i1: i, i2: i + 1)
+            if colDiff == 1 {
+                return i + 1
+            }
+        }
+
+        return 0
+    }
+
+    private func getColDiff(block: [String], i1: Int, i2: Int) -> Int {
+        var col1 = i1
+        var col2 = i2
+        var diff = 0
+        outer: while col1 >= 0 && col2 < block[0].count {
+            for j in 0 ..< block.count {
+                let row = block[j]
+                if row[col1] != row[col2] {
+                    diff += 1
+                }
+            }
+            col1 -= 1
+            col2 += 1
+        }
+
+        return diff
+    }
+
+    private func getRowDiff(block: [String], i1: Int, i2: Int) -> Int {
+        var i1 = i1
+        var i2 = i2
+        var diff = 0
+        while i1 >= 0 && i2 < block.count {
+            for j in 0 ..< block[i1].count {
+                if block[i1][j] != block[i2][j] {
+                    diff += 1
+                }
+            }
+            i1 -= 1
+            i2 += 1
+        }
+        return diff
     }
 }
 
