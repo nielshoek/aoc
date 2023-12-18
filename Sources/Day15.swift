44286dc7ce16495713511bbeb8698e5ae5e5771e
Niels Hoek <njhoek@gmail.com>
2023-12-18 23:54:37 +0100

    Day 15 part 2

--- Sources/Day15.swift
+++ Sources/Day15.swift
@@ -1,11 +1,12 @@
 let test = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+let testB = "rn,cm,qp,cm,qp,pc,ot,ab,pc,pc,ot"
 
 public struct Day15 {
     public func Run() {
         let data = "Inputs/day15.txt".ToString()
         print("Part TEST: \(LogicA(input: test))")
         print("Part 1: \(LogicA(input: data))")
-        // print("Part 2: \(LogicB(input: data))")
+        print("Part 2: \(LogicB(input: data))")
     }
 
     public func LogicA(input: String) -> Int {
@@ -19,7 +20,39 @@ public struct Day15 {
         }.reduce(0) { $0 + $1 }
     }
 
-    public func LogicB(input _: String) -> Int {
-        0
+    public func LogicB(input: String) -> Int {
+        var boxesMap = [[(label: String, strength: String)]](repeating: [(label: String, strength: String)](), count: 256)
+        let steps = input.components(separatedBy: ",")
+        for step in steps {
+            let action = step.contains("=") ? "ASSIGN" : "REMOVE"
+            let parts = step.components(separatedBy: ["-", "="])
+            let label = parts[0]
+            let strength = parts[1]
+            let labelValue = label.map(Character.init).reduce(into: 0) { acc, cur in
+                acc += Int(cur.asciiValue!)
+                acc *= 17
+                acc %= 256
+            }
+            var box = boxesMap[Int(labelValue)]
+            if action == "ASSIGN" {
+                if let i = box.firstIndex(where: { $0.label == label }) {
+                    box[i] = (label: label, strength: strength)
+                } else {
+                    box.append((label: label, strength: strength))
+                }
+            } else if action == "REMOVE" {
+                box.removeAll(where: { $0.label == label })
+            }
+            boxesMap[Int(labelValue)] = box
+        }
+
+        var count = 0
+        for (i, boxes) in boxesMap.enumerated() {
+            for (j, lens) in boxes.enumerated() {
+                count += (i + 1) * (j + 1) * Int(lens.strength)!
+            }
+        }
+
+        return count
     }
 }
