f030aef9037150b1efae9e866e236164cb8f8874
Niels Hoek <njhoek@gmail.com>
2023-12-22 18:55:15 +0100

    Day 19 part 2 (WIP)

--- Sources/Day19.swift
+++ Sources/Day19.swift
@@ -1,7 +1,9 @@
+import DequeModule
+
 public struct Day19 {
     public func Run() {
         let data = "Inputs/day19.txt".ToStringArray()
-        print("Part 1: \(LogicA(input: data))")
+        // print("Part 1: \(LogicA(input: data))")
         print("Part 2: \(LogicB(input: data))")
     }
 
@@ -47,6 +49,122 @@ public struct Day19 {
         return acceptedParts.reduce(0) { $0 + $1.values.values.reduce(0, +) }
     }
 
+    public func LogicB(input: [String]) -> Int {
+        let workflows = generateWorkflows(strings: input.split(separator: "")[0])
+        var acceptedRanges = [PartRanges]()
+
+        let startRanges = PartRanges(x: 1 ... 4000, m: 1 ... 4000, a: 1 ... 4000, s: 1 ... 4000)
+        var queue = Deque<(name: String, ranges: PartRanges)>(arrayLiteral: ("in", startRanges))
+        while let (workflowName, ranges) = queue.popFirst() {
+            if workflowName == "A" {
+                acceptedRanges.append(ranges)
+                continue
+            }
+            if workflowName == "R" {
+                continue
+            }
+
+            let workflow = workflows[workflowName]!
+            let flows = workflow.flows
+            var ranges = ranges
+            for (char, flow) in flows {
+                switch char {
+                case "x":
+                    var xRange: ClosedRange<Int>
+                    if flow.sign == "<" && ranges.x.lowerBound < flow.value {
+                        xRange = ranges.x.lowerBound ... min(max(flow.value - 1, ranges.x.lowerBound), ranges.x.upperBound)
+                        ranges.x = max(min(flow.value, ranges.x.upperBound), ranges.x.lowerBound) ... ranges.x.upperBound
+                    } else if flow.sign == ">" {
+                        xRange = max(min(flow.value + 1, ranges.x.upperBound), ranges.x.lowerBound) ... ranges.x.upperBound
+                        ranges.x = ranges.x.lowerBound ... min(max(flow.value, ranges.x.lowerBound), ranges.x.upperBound)
+                    } else {
+                        continue
+                    }
+                    let newRanges = PartRanges(x: xRange,
+                                               m: ranges.m,
+                                               a: ranges.a,
+                                               s: ranges.s)
+                    queue.append((flow.next, newRanges))
+                case "m":
+                    var mRange: ClosedRange<Int>
+                    if flow.sign == "<" && ranges.m.lowerBound < flow.value {
+                        mRange = ranges.m.lowerBound ... min(max(flow.value - 1, ranges.m.lowerBound), ranges.m.upperBound)
+                        ranges.m = max(min(flow.value, ranges.m.upperBound), ranges.m.lowerBound) ... ranges.m.upperBound
+                    } else if flow.sign == ">" {
+                        mRange = max(min(flow.value + 1, ranges.m.upperBound), ranges.m.lowerBound) ... ranges.m.upperBound
+                        ranges.m = ranges.m.lowerBound ... min(max(flow.value, ranges.m.lowerBound), ranges.m.upperBound)
+                    } else {
+                        continue
+                    }
+                    let newRanges = PartRanges(x: ranges.x,
+                                               m: mRange,
+                                               a: ranges.a,
+                                               s: ranges.s)
+                    queue.append((flow.next, newRanges))
+                case "a":
+                    var aRange: ClosedRange<Int>
+                    if flow.sign == "<" && ranges.a.lowerBound < flow.value {
+                        aRange = ranges.a.lowerBound ... min(max(flow.value - 1, ranges.a.lowerBound), ranges.a.upperBound)
+                        ranges.a = max(min(flow.value, ranges.a.upperBound), ranges.a.lowerBound) ... ranges.a.upperBound
+                    } else if flow.sign == ">" {
+                        aRange = max(min(flow.value + 1, ranges.a.upperBound), ranges.a.lowerBound) ... ranges.a.upperBound
+                        ranges.a = ranges.a.lowerBound ... min(max(flow.value, ranges.a.lowerBound), ranges.a.upperBound)
+                    } else {
+                        continue
+                    }
+                    let newRanges = PartRanges(x: ranges.x,
+                                               m: ranges.m,
+                                               a: aRange,
+                                               s: ranges.s)
+                    queue.append((flow.next, newRanges))
+                case "s":
+                    var sRange: ClosedRange<Int>
+                    if flow.sign == "<" && ranges.s.lowerBound < flow.value {
+                        sRange = ranges.s.lowerBound ... min(max(flow.value - 1, ranges.s.lowerBound), ranges.s.upperBound)
+                        ranges.s = max(min(flow.value, ranges.s.upperBound), ranges.s.lowerBound) ... ranges.s.upperBound
+                    } else if flow.sign == ">" {
+                        sRange = max(min(flow.value + 1, ranges.s.upperBound), ranges.s.lowerBound) ... ranges.s.upperBound
+                        ranges.s = ranges.s.lowerBound ... min(max(flow.value, ranges.s.lowerBound), ranges.s.upperBound)
+                    } else {
+                        continue
+                    }
+                    let newRanges = PartRanges(x: ranges.x,
+                                               m: ranges.m,
+                                               a: ranges.a,
+                                               s: sRange)
+                    queue.append((flow.next, newRanges))
+                default:
+                    if flow.next != "A" || flow.next != "R" {
+                        queue.append((flow.next, ranges))
+                    } else if flow.sign == "A" {
+                        acceptedRanges.append(ranges)
+                    }
+                }
+            }
+        }
+
+        let uniqueRanges = acceptedRanges.unique
+        let result = uniqueRanges.reduce(0) { acc, cur in
+            acc + (cur.x.upperBound + 1 - cur.x.lowerBound)
+                * (cur.m.upperBound + 1 - cur.m.lowerBound)
+                * (cur.a.upperBound + 1 - cur.a.lowerBound)
+                * (cur.s.upperBound + 1 - cur.s.lowerBound)
+        }
+
+        return result
+        // 14372248406663
+        // 131550418841958      C
+        // 143722484066630
+        // 126026760036084
+    }
+
+    struct PartRanges: Equatable {
+        var x: ClosedRange<Int>
+        var m: ClosedRange<Int>
+        var a: ClosedRange<Int>
+        var s: ClosedRange<Int>
+    }
+
     struct Workflow {
         let name: String
         let flows: [(char: Character?, flow: Flow)]
@@ -56,6 +174,8 @@ public struct Day19 {
         let char: Character?
         let condition: (Int) -> Bool
         let next: String
+        let sign: String
+        let value: Int
     }
 
     struct Part {
@@ -72,26 +192,33 @@ public struct Day19 {
                 if flowParts.count == 1 {
                     return Flow(char: nil,
                                 condition: { _ in true },
-                                next: String(flowParts[0]))
+                                next: String(flowParts[0]),
+                                sign: "_",
+                                value: 0)
                 }
                 var char: Character = " "
                 var condition: (Int) -> Bool = { _ in false }
+                var sign = ""
+                var value: Int = -1
                 if flowParts[0].contains("<") {
+                    sign = "<"
                     char = Character(String(flowParts[0].split(separator: "<")[0]))
                     let otherNr = flowParts[0].split(separator: "<")[1]
+                    value = Int(otherNr)!
                     condition = { nr in
-                        nr < Int(otherNr)!
+                        nr < value
                     }
                 } else if flowParts[0].contains(">") {
-                    let ss = String(flowParts[0].split(separator: ">")[0])
-                    char = Character(ss)
+                    sign = ">"
+                    char = Character(String(flowParts[0].split(separator: ">")[0]))
                     let otherNr = flowParts[0].split(separator: ">")[1]
+                    value = Int(otherNr)!
                     condition = { nr in
-                        nr > Int(otherNr)!
+                        nr > value
                     }
                 }
                 let next = String(flowParts[1])
-                return Flow(char: char, condition: condition, next: next)
+                return Flow(char: char, condition: condition, next: next, sign: sign, value: value)
             }
 
             return Workflow(name: workflowName,
@@ -111,8 +238,15 @@ public struct Day19 {
             return Part(values: values)
         }
     }
+}
 
-    public func LogicB(input _: [String]) -> Int {
-        0
+extension Array where Element: Equatable {
+    var unique: [Element] {
+        var uniqueValues: [Element] = []
+        forEach { item in
+            guard !uniqueValues.contains(item) else { return }
+            uniqueValues.append(item)
+        }
+        return uniqueValues
     }
 }
