3d158f8a8e26ffee8c615bb3a1f5d5fb9a7d3f9e
Niels Hoek <njhoek@gmail.com>
2024-12-12 22:50:39 +0100

    Day 12 part 2

--- 2024/aoc/Sources/Solutions/Day12.swift
+++ 2024/aoc/Sources/Solutions/Day12.swift
@@ -63,21 +63,99 @@ class Day12 {
 
     func B() -> Int {
         var grid = FileHelpers.ReadFileToStringArray(path: "Sources/Inputs/day12.txt")
-            .map(Array.init)
+            .map { ["_"] + Array($0) + ["_"] }
+        grid = [[Character](repeating: "_", count: grid[0].count)] + grid
+        grid.append([Character](repeating: "_", count: grid[0].count))
 
         var result = 0
         var visited = [[Bool]](
             repeating: [Bool](repeating: false, count: grid[0].count),
             count: grid.count)
-        for r in 0..<grid.count {
-            for c in 0..<grid[0].count {
+        for r in 1..<grid.count-1 {
+            for c in 1..<grid[0].count-1 {
                 if visited[r][c] {
                     continue
                 }
-                
+                let char = grid[r][c]
+                let area = flood(&grid, &visited, r, c, char, "*")
+
+                var leftBorders = [[Bool]](
+                    repeating: [Bool](repeating: false, count: grid[0].count),
+                    count: grid.count)
+                var rightBorders = [[Bool]](
+                    repeating: [Bool](repeating: false, count: grid[0].count),
+                    count: grid.count)
+                var topBorders = [[Bool]](
+                    repeating: [Bool](repeating: false, count: grid[0].count),
+                    count: grid.count)
+                var bottomBorders = [[Bool]](
+                    repeating: [Bool](repeating: false, count: grid[0].count),
+                    count: grid.count)
+                var perimeter = 0
+                for r in 1..<grid.count {
+                    var last = Character("_")
+                    for c in 1..<grid[0].count {
+                        if last != "*" && grid[r][c] == "*" {
+                            // left border
+                            leftBorders[r][c] = true
+                            if !leftBorders[r-1][c] {
+                                perimeter += 1
+                            }
+                        } else if last == "*" && grid[r][c] != "*" {
+                            // right border
+                            rightBorders[r][c-1] = true
+                            if !rightBorders[r-1][c-1] {
+                                perimeter += 1
+                            }
+                        }
+                        last = grid[r][c]
+                    }
+                }
+                for c in 1..<grid[0].count {
+                    var last = Character("_")
+                    for r in 1..<grid.count {
+                        if last != "*" && grid[r][c] == "*" {
+                            // top border
+                            topBorders[r][c] = true
+                            if !topBorders[r][c-1] {
+                                perimeter += 1
+                            }
+                        } else if last == "*" && grid[r][c] != "*" {
+                            // bottom border
+                            bottomBorders[r-1][c] = true
+                            if !bottomBorders[r-1][c-1] {
+                                perimeter += 1
+                            }
+                        }
+                        last = grid[r][c]
+                    }
+                }
+                result += area * perimeter
+                _ = flood(&grid, &visited, r, c, "*", char)
             }
         }
 
         return result
+    }
+
+    func flood(
+        _ grid: inout [[Character]], _ visited: inout [[Bool]], _ r: Int, _ c: Int,
+        _ char: Character, _ replacement: Character
+    ) -> Int {
+        if !grid.indices.contains(r) || !grid[0].indices.contains(c) {
+            return 0
+        }
+        if grid[r][c] != char {
+            return 0
+        }
+        visited[r][c] = true
+        grid[r][c] = replacement
+        let dirs = [0, 1, 0, -1, 0]
+        var count = 1
+        for i in 1..<dirs.count {
+            let (r, c) = (r + dirs[i - 1], c + dirs[i])
+            count += flood(&grid, &visited, r, c, char, replacement)
+        }
+        return count
     }
 }
